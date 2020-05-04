3986b6565f0f6f0cef7c5f23ed90a8d9eb4642f2
staneee <yi.hang@live.com>
2020-05-04 22:03:04 +0800

    增加程序集对动态webapi的配置

---
+++ src/Panda.DynamicWebApi/AssemblyDynamicWebApiOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace Panda.DynamicWebApi
+{
+    /// <summary>
+    /// Specifies the dynamic webapi options for the assembly.
+    /// </summary>
+    public class AssemblyDynamicWebApiOptions
+    {
+        /// <summary>
+        /// Routing prefix for all APIs
+        /// <para></para>
+        /// Default value is "api".
+        /// </summary>
+        public string ApiPrefix { get; set; }
+
+        /// <summary>
+        /// API HTTP Verb.
+        /// <para></para>
+        /// Default value is "POST".
+        /// </summary>
+        public string HttpVerb { get; set; }
+    }
+}
